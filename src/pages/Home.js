02a0cc3548c012ff996bbf493005363551f939a0
= <salaslilia00@gmail.com>
2021-08-08 23:54:03 -0400

    created home file

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,5 +1,31 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 
 export default function Home(props) {
-	return <div className="HomePage">This is the {props.page} page</div>;
+	const [bookmarks, setBookmarks] = useState([]);
+
+	useEffect(() => {
+		(async () => {
+			try {
+				const response = await fetch('/api/bookmarks');
+				const data = await response.json();
+				setBookmarks(data);
+			} catch (error) {
+				console.error(error);
+			}
+		})();
+	}, []);
+
+	return (
+		<div className="HomePage">
+			<ul>
+				{bookmarks.map(bookmark => {
+					return (
+						<li key={bookmark._id}>
+							<a href={bookmark.link}>{bookmark.title}</a>
+						</li>
+					);
+				})}
+			</ul>
+		</div>
+	);
 }
